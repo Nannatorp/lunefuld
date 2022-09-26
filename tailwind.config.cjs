da3c541522947eb0a2c9c5aaf910c91255e113f6
Nanna Hansen <nann257j@stud.kea.dk>
2022-09-26 17:01:46 +0200

    Tilføjet font til hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        futura: "'futura-pt', sans-serif",
+        blakely: "'blakely', sans-serif;",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
